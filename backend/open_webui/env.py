29d582dad91f61918cba7d3fb9e56720011872c8
Rafael <101749351+TechBeme@users.noreply.github.com>
2025-04-02 03:14:05 -0300

    Atualizar env.py Remover (Open WebUI) e alterar favicon url

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -105,11 +105,9 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "YuIA")
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://res.cloudinary.com/dk2npn4hi/image/upload/v1741578790/favicon.png"
 
 TRUSTED_SIGNATURE_KEY = os.environ.get("TRUSTED_SIGNATURE_KEY", "")
 
