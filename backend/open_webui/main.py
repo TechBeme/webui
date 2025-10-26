a79f7b70d293722442951d50075888c2501a6428
TechBeme <101749351+TechBeme@users.noreply.github.com>
2025-10-26 13:08:16 -0300

    Brand: Alterar a descrição do manifest.json.

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -2332,7 +2332,7 @@ async def get_manifest_json():
         return {
             "name": app.state.WEBUI_NAME,
             "short_name": app.state.WEBUI_NAME,
-            "description": f"{app.state.WEBUI_NAME} is an open, extensible, user-friendly interface for AI that adapts to your workflow.",
+            "description": f"A YuIA é uma plataforma que reúne as melhores ferramentas de inteligência artificial — como ChatGPT, Gemini, Claude, Grok e muito mais — em um só lugar!",
             "start_url": "/",
             "display": "standalone",
             "background_color": "#343541",
