867bb3b7890f743c1297ca5681d9386c602659ee
TechBeme <101749351+TechBeme@users.noreply.github.com>
2026-01-23 17:03:59 -0300

    Rebrand: Alterar o nome do Channel de 'Open WebUI' para 'YuIA'

--- src/lib/components/channel/Channel.svelte
+++ src/lib/components/channel/Channel.svelte
@@ -284,10 +284,10 @@
 					} else {
 						return e.name;
 					}
-				}, '')} • Open WebUI</title
+				}, '')} • YuIA</title
 		>
 	{:else}
-		<title>#{channel?.name ?? 'Channel'} • Open WebUI</title>
+		<title>#{channel?.name ?? 'Channel'} • YuIA</title>
 	{/if}
 </svelte:head>
 
